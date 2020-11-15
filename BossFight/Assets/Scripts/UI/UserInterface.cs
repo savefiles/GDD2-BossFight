fbe749ff23b30e91e5f63989da1dd5150b0eb8e7
Chris Hambacher <hambcastlesen12@gmail.com>
2020-11-15 17:08:21 -0500

    Fixed conflicts with the Player class

---
+++ BossFight/Assets/Scripts/UI/UserInterface.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Monobehaviour attached to the player game object
+// - Holds references to the non-monobehavior scripts.
+// - Might be a good idea to have the player as a prefab?
+
+public class UserInterface : MonoBehaviour
+{
+    // References
+    GameManager gManager;
+
+    // Health related things
+    public float health = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get scene references.
+        gManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
