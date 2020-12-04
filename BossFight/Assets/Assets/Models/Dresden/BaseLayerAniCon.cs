d2b044fe27235aa4673a1f51bd4955b9b51f310f
bjl4618 <bjl4618@g.rit.edu>
2020-12-04 15:17:57 -0500

    fixed cloth and added new version of dresden model, working on getting animations hooked up to inputs

---
+++ BossFight/Assets/Assets/Models/Dresden/BaseLayerAniCon.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AniFacts //Data user needs to decide for each animation
+{
+    public bool neutral = true; 
+    public string name; 
+    public int end = 60; 
+    public int nextAni = -1; 
+    public float transitionSpeed = 0; 
+}
+
+public class BaseLayerAniCon : MonoBehaviour //Script that performs the heavylifting for playing our animations
+{
+    public static float aFrame = 1 / 60f; //How much time passes in a frame, if we assume a frame is 1/60th of a second
+    public static float bFrame = 24 / 60f; //Frame rate adjusted from Blender
+    public Animator aniConRef; 
+    public int curentAnimation; 
+    public float frame; 
+    public List<AniFacts> actions = new List<AniFacts>() { new AniFacts() }; 
+
+    public void Start() //Sets default animation to idle and decides speed
+    {
+        Change(0,0); 
+        aniConRef.enabled = false;
+        Play(1); 
+    }
+
+    public void Change(int targetAniamtion = 0, float transitionSpeed = 0) //Command called to change which Animation is playing
+    {
+        curentAnimation = targetAniamtion; 
+        frame = 0; 
+        if (transitionSpeed <= 0) aniConRef.PlayInFixedTime(actions[curentAnimation].name); 
+        else aniConRef.CrossFadeInFixedTime(actions[curentAnimation].name, transitionSpeed); 
+    }
+
+    public bool Play(float rate) //Main method that runs/loops/triggers Animations
+    {
+        aniConRef.Update(rate * aFrame); 
+        frame += rate * bFrame; //Adjusts frame based on Blender's frame rate
+
+        bool over = false; 
+        if (actions[curentAnimation].nextAni < 0) over = false; 
+        else if (frame >= actions[curentAnimation].end) 
+        {
+            int indexOLD = curentAnimation; 
+            curentAnimation = actions[curentAnimation].nextAni; 
+            Change(actions[indexOLD].nextAni, actions[indexOLD].transitionSpeed); 
+            frame = 0; 
+            over = true; 
+        }
+        return over;
+    }
+}
---
+++ BossFight/Assets/Assets/Models/Dresden/TutorialExample.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialExample : MonoBehaviour
+{
+    public BaseLayerAniCon aniCon;
+    public float yTarget;
+    public float xTarget;
+
+    private void Start()
+    {
+        aniCon.Start();
+    }
+
+    void Update()
+    {
+        aniCon.Play(1);
+
+        if (aniCon.actions[aniCon.curentAnimation].neutral) //If Current Animation = A neutral action
+        {
+            if (Input.GetKeyDown(KeyCode.Q)) //If you press y do this aniamtion
+            {
+                aniCon.Change(2, .4f);
+            }
+            else if (Input.GetKey(KeyCode.W)) //If you press Up Arrow, Activate walking forward
+            {
+                if (aniCon.curentAnimation != 1) aniCon.Change(1, .3f);
+                yTarget = Mathf.MoveTowards(yTarget, 1, .05f);
+            }
+            else if (Input.GetKey(KeyCode.S)) //If you press Up Arrow, Activate walking forward
+            {
+                if (aniCon.curentAnimation != 1) aniCon.Change(1, .3f);
+                yTarget = Mathf.MoveTowards(yTarget, -1, .05f);
+            }
+            else if (Input.GetKey(KeyCode.A)) //If you press Up Arrow, Activate walking forward
+            {
+                if (aniCon.curentAnimation != 1) aniCon.Change(1, .3f);
+                xTarget = Mathf.MoveTowards(xTarget, -1, .05f);
+            }
+            else if (Input.GetKey(KeyCode.D)) //If you press Up Arrow, Activate walking forward
+            {
+                if (aniCon.curentAnimation != 1) aniCon.Change(1, .3f);
+                xTarget = Mathf.MoveTowards(xTarget, 1, .05f);
+            }
+            else //Otherwise, return to idle
+            {
+                if (aniCon.curentAnimation != 0) aniCon.Change(0, .4f);
+                yTarget = Mathf.MoveTowards(yTarget, 0, .05f);
+            }
+        }
+
+        aniCon.aniConRef.SetFloat("y", yTarget);
+        aniCon.aniConRef.SetFloat("x", xTarget);
+    }
+}
