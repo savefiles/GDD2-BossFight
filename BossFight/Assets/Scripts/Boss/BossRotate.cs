de6fd56505aebecbe3d3327f0589237546fca86d
cag5637 <cag5637@rit.edu>
2020-12-08 13:33:25 -0600

    Added BossModel and BossRotate

---
+++ BossFight/Assets/Scripts/Boss/BossRotate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class BossRotate : MonoBehaviour {
+
+    public GameObject playerRef;
+
+    void Start() {
+        playerRef = GameObject.Find("Player").transform.GetChild(0).gameObject;
+    }
+
+    void FixedUpdate() {
+        transform.LookAt(playerRef.transform);
+        Debug.Log("Looked");
+    }
+}
